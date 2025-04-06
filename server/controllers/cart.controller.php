471b55a7684ab006187e06d6f056d81b710dbf2e
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-04-06 22:02:59 +0200

    feat: enhance cart functionality with user validation and additional methods for retrieving and updating cart items

--- server/controllers/cart.controller.php
+++ server/controllers/cart.controller.php
@@ -3,6 +3,7 @@
 require_once '../utils/HelperTrait.php';
 require_once '../middlewares/validator.middleware.php';
 require_once '../models/Cart.php';
+require_once '../models/User.php';
 require_once '../models/CartProduct.php';
 class CartController {
     use HelperTrait;
@@ -13,7 +14,10 @@ class CartController {
         $method = $_SERVER['REQUEST_METHOD'];
 
         switch ($method) {
-			
+			case 'GET':
+                $this->getCart($id);
+			case 'PATCH':
+                $this->updateCart($id);
 			case 'POST':
 				$this->createCart();
 			case 'DELETE':
@@ -64,6 +68,10 @@ class CartController {
     }
 
     private function removeProductFromCart($id){
+        $user = User::find($id);
+        if (!$user) {
+            $this->apiResponse((object)[], 'User not found', 404);
+		}
         $jsonData = json_decode(file_get_contents("php://input"), true);
         
 		$validator = Validator::make($jsonData, [
@@ -72,7 +80,7 @@ class CartController {
 		if ($validator->fails()) {
 			$this->apiResponse((object)[], $validator->firstError(), 404);
 		}
-        $cart = Cart::find($id);
+        $cart = Cart::where('user_id', '=',$id)->first();
         if (!$cart) {
             $this->apiResponse((object)[], 'Cart not found', 404);
 		}
@@ -84,6 +92,52 @@ class CartController {
         }else {
             $this->apiResponse((object)[], 'Product not existed in the cart', 404);
         }
+    }
+
+    private function getCart($id){
+        $user = User::find($id);
+        if (!$user) {
+            $this->apiResponse((object)[], 'User not found', 404);
+		}
+        $cart = Cart::where('user_id', '=',$id)->first();
+        if (!$cart){
+            $this->apiResponse([], 'ok', 200);
+        }else {
+            $cartProducts = CartProduct::where('cart_id', '=', $cart['id'])->get();
+            $this->apiResponse($cartProducts, 'ok', 200);
+        }
+    }
+    
+    private function updateCart($id){
+        $user = User::find($id);
+        if (!$user) {
+            $this->apiResponse((object)[], 'User not found', 404);
+		}
+        $cart = Cart::where('user_id', '=',$id)->first();
+        if (!$cart){
+            $this->apiResponse((object)[], 'cart not found', 404);
+        }
+        
+        $jsonData = json_decode(file_get_contents("php://input"), true);
+        
+		$validator = Validator::make($jsonData, [
+            'quantity' => 'required|numeric',
+			'product_id' => 'required|numeric|exists:products',
+		]);
+		if ($validator->fails()) {
+            $this->apiResponse((object)[], $validator->firstError(), 404);
+		}
+        if ($jsonData['quantity'] <= 0 ){
+            $this->apiResponse((object)[], 'Product quantity must be greater than zero', 404);
+        }
+        $cartProduct = CartProduct::where('cart_id', '=',$cart['id'])->where('product_id', '=',$jsonData['product_id'])->first();
+        if (!$cartProduct){
+            $this->apiResponse((object)[], 'Product not found in the cart', 404);
+        }
+        CartProduct::update($cartProduct['id'], [
+            'quantity' => $jsonData['quantity']
+        ]);
+        $this->apiResponse((object)[], 'ok', 200);
     }
 }
 
